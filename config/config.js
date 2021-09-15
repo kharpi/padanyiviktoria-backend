b1c731bd9d71166155451064ebf98620609a598a
Kárpáti Tamás <tamas.nandor.karpati@gmail.com>
2021-09-15 23:52:11 +0200

    changed sequelize config

--- config/config.js
+++ config/config.js
@@ -2,13 +2,10 @@ require('dotenv').config();
 module.exports = {
 	development: {
 		dialect: 'sqlite',
-		storage: 'data.sqlite3',
+		storage: 'data.test.sqlite3',
 	},
 	production: {
-		username: process.env.PROD_DB_USERNAME,
-		password: process.env.PROD_DB_PASSWORD,
-		database: process.env.PROD_DB_NAME,
-		host: process.env.PROD_DB_HOSTNAME,
-		dialect: 'mysql',
+		dialect: 'sqlite',
+		storage: 'data.sqlite3',
 	},
 };
